77a1cd24c19d445f5ba1c1a3fada4ae443ee7efb
Nick Fletcher <nickeax@gmail.com>
2024-11-16 10:02:36 +1100

    Add synch project.

---
+++ ThreadSynchronisation/Program.cs
@@ -0,0 +1,22 @@
+﻿int counter = 0;
+object lockObject = new();
+
+Thread thread1 = new(() => IncrementCounter());
+Thread thread2 = new(() => IncrementCounter());
+thread1.Start();
+thread2.Start();
+thread1.Join();
+thread2.Join();
+
+Console.WriteLine(counter);
+
+void IncrementCounter()
+{
+    for (int i = 0; i < 100000; i++)
+    {
+        lock (lockObject)
+        {
+            counter++;
+        }
+    }
+}
